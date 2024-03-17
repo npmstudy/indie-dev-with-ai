4a0b816ff050ead161af3cf07775e811e6095536
npmstudy <npmstudy@qq.com>
2024-03-17 18:11:24 +0800

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import vercel from "@astrojs/vercel/serverless";
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   integrations: [
     starlight({
       title: "Indie dev",
